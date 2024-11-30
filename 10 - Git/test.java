3fcea86d00686edb35bfacc88397fac75c0d77de
baraa207 <baraakadri12@gmail.com>
2024-11-30 13:52:06 -0500

    ! ajouté

--- 10 - Git/test.java
+++ 10 - Git/test.java
@@ -1,11 +1,11 @@
+
 import java.util.Scanner;
 
 public class test {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("bonjour monde");
+        System.out.println("Bonjour !");
 
         sc.close();
     }
